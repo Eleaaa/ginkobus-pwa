0b0c7a53c22c16267b4090b2124ef4d7f05daae6
Eleaaa <60519357+Eleaaa@users.noreply.github.com>
2022-11-28 11:32:54 +0100

    h

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
 const cacheName = 'ginkobus-v1';
-const appShellFiles = [
+const appFiles = [
   'index.html',
   'app.js',
   'style.css',
@@ -17,3 +13,12 @@ const appShellFiles = [
   'icons/icon-256.png',
   'icons/icon-512.png'
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(appFiles);
+    })());
+  });
