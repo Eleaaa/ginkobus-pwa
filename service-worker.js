4b6ca5fb19893776e5327d8bea9ea90027ee427a
Eleaaa <60519357+Eleaaa@users.noreply.github.com>
2022-11-28 12:12:57 +0100

    o

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('install', (e) => {
     })());
   });
 
-  self.addEventListener('fetch', (e) => {
+  /*self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
       const r = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
@@ -35,4 +35,18 @@ self.addEventListener('install', (e) => {
       cache.put(e.request, response.clone());
       return response;
     })());
+  });*/
+
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          var fetchPromise = fetch(event.request).then(function (networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
+        });
+      }),
+    );
   });
