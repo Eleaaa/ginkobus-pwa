6f671716bc69b98b12e767fa92aa883c69910a2a
Eleaaa <60519357+Eleaaa@users.noreply.github.com>
2022-11-28 11:43:03 +0100

    k

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ let URL = "https://ginkobus-server.onrender.com";
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js');
+    }
+
     
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
