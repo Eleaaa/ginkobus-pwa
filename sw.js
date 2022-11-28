677afa8d6264cbef0959e56ec4f4250fae7fa990
Eleaaa <60519357+Eleaaa@users.noreply.github.com>
2022-11-28 11:31:25 +0100

    g

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
-  });
+});
+
+const cacheName = 'ginkobus-v1';
+const appShellFiles = [
+  'index.html',
+  'app.js',
+  'style.css',
+  'icons/favicon.ico',
+  'icons/icon-32.png',
+  'icons/icon-64.png',
+  'icons/icon-96.png',
+  'icons/icon-128.png',
+  'icons/icon-168.png',
+  'icons/icon-192.png',
+  'icons/icon-256.png',
+  'icons/icon-512.png'
+];
